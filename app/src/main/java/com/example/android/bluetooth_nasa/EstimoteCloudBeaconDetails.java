5aebffc0e5e46bc0c0acae14224ce4e85fc571f0
DavLiet <davlietjauw@yahoo.com>
2017-07-15 00:53:25 -0500

    added EstimoteCloudBeaconDetails class

---
+++ app/src/main/java/com/example/android/bluetooth_nasa/EstimoteCloudBeaconDetails.java
@@ -0,0 +1,31 @@
+package com.example.android.bluetooth_nasa;
+
+/**
+ * Created by David Lie-Tjauw on 7/15/2017.
+ */
+
+import com.estimote.sdk.cloud.model.Color;
+
+public class EstimoteCloudBeaconDetails {
+
+    private String beaconName;
+    private Color beaconColor;
+
+    public EstimoteCloudBeaconDetails(String beaconName, Color beaconColor) {
+        this.beaconName = beaconName;
+        this.beaconColor = beaconColor;
+    }
+
+    public String getBeaconName() {
+        return beaconName;
+    }
+
+    public Color getBeaconColor() {
+        return beaconColor;
+    }
+
+    @Override
+    public String toString() {
+        return "[beaconName: " + getBeaconName() + ", beaconColor: " + getBeaconColor() + "]";
+    }
+}
